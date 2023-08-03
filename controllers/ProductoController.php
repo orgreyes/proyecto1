f2b2cd428368a43c63a446c446ce7bb24269abaf
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-08-03 13:28:18 -0600

    Primea Clase de MVC

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -7,11 +7,11 @@ use MVC\Router;
 class ProductoController{
     public static function index(Router $router){
         $productos = Producto::all();
+        // $productos2 = Producto::all();
 
-        var_dump($productos);
-        exit;
-        
-
-        $router->render('productos/index'); 
+        $router->render('productos/index', [
+            'productos' => $productos,
+            // 'productos2' => $productos2,
+        ]); 
     }
 }
--- views/productos/index.php
+++ views/productos/index.php
@@ -1,1 +1,7 @@
 <h1>Vista de Productos</h1>
+
+<?php foreach($productos as $key => $producto): ?>
+
+    <li><?= $producto->producto_nombre . $producto->producto_id?></li>
+
+    <?php endforeach ?>;
